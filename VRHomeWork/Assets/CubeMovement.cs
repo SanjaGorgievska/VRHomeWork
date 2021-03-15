ebe3b3784c971f3fa23d465d2372d0da924a07c6
Sanja Gorgievska <gorgievskasanja66@gmail.com>
2021-03-15 17:36:20 +0100

    Inital

---
+++ VRHomeWork/Assets/CubeMovement.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CubeMovement : MonoBehaviour
+{
+    NavMeshAgent myAgent;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+        myAgent.speed = speed;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            myAgent.destination = hit.point;
+        }
+    }
+}
